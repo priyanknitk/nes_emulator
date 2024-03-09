eac20c817a45f2b8e32079235092591a804c6d56
Priyank Mandavawala <primanda@microsoft.com>
2024-03-09 08:30:48 +0530

    Add memory management and new instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,6 +3,7 @@ pub struct CPU {
     pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xffff],
 }
 
 impl CPU {
@@ -12,24 +13,63 @@ impl CPU {
             register_x: 0,
             status: 0,
             program_counter: 0,
+            memory: [0; 0xffff],
         }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
-        self.program_counter = 0;
+    fn mem_read(&self, address: u16) -> u8 {
+        self.memory[address as usize]
+    }
 
+    fn mem_write(&mut self, address: u16, value: u8) {
+        self.memory[address as usize] = value;
+    }
+
+    fn mem_read_u16(&mut self, pos: u16) -> u16 {
+        let lo = self.mem_read(pos) as u16;
+        let hi = self.mem_read(pos + 1) as u16;
+        hi << 8 | (lo as u16)
+    }
+
+    fn mem_write_u16(&mut self, pos: u16, data: u16) {
+        let hi = (data >> 8) as u8;
+        let lo = (data & 0xff) as u8;
+        self.mem_write(pos, lo);
+        self.mem_write(pos + 1, hi);
+    }
+
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
+        self.load(program);
+        self.reset();
+        self.run();
+    }
+
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000..0x8000 + program.len()].copy_from_slice(&program);
+        self.mem_write_u16(0xFFFC, 0x8000);
+    }
+
+    pub fn reset(&mut self) {
+        self.register_a = 0;
+        self.register_x = 0;
+        self.status = 0;
+        self.program_counter = self.mem_read_u16(0xFFFC); 
+    }
+
+    pub fn run(&mut self) {
         loop {
-            let opscode = program[self.program_counter as usize];
+            let opscode = self.mem_read(self.program_counter);
             self.program_counter += 1;
 
             match opscode {
                 0x00 => return,
                 0xA9 => {
-                    let param = program[self.program_counter as usize];
-                    self.program_counter +=1;
+                    let param = self.mem_read(self.program_counter);
+                    self.program_counter += 1;
                     self.lda(param);
                 },
-                0xAA =>  self.tax(),
+                0xAA => self.tax(),
+                0xE8 => self.inx(),
                 _ => todo!()
             }
         }
@@ -42,6 +82,11 @@ impl CPU {
 
     fn tax(&mut self) {
         self.register_x = self.register_a;
+        self.update_zero_and_negative_flags(self.register_x);
+    }
+
+    fn inx(&mut self) {
+        self.register_x = self.register_x.wrapping_add(1);
         self.update_zero_and_negative_flags(self.register_x);
     }
 
